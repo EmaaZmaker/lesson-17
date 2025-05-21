f78eb13bc8801890f0e8b5bd53cbb66969b058e9
EmaaZmaker <emaazbinammar@gmail.com>
2025-05-21 19:16:06 +0400

    rrgrg

---
+++ apy.py
@@ -0,0 +1,10 @@
+def shut_down(no,yes):
+    return choice
+print("a.no")
+print("b.yes")
+choice=input(("do you want to shut down?"))
+if choice=="a":
+    print("Abort shutdown")
+if choice=="b":
+    print("shutting down...")
+'mam i dont understand how to use the arguments for this project.can u explain how in the next class?v'
